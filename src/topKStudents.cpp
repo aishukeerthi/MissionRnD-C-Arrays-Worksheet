244bb8e409aa9a88cb23ae4465376743dc92ba58
aishukeerthi <aishukeerthi@missionrnd.com>
2016-03-13 22:48:25 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -15,12 +15,56 @@ NOTES:
 
 #include <iostream>
 #include <malloc.h>
+#include<stdio.h>
 
 struct student {
 	char *name;
 	int score;
 };
+void quicksort(struct student *, int, int);
 
 struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	if (len <= 0 || K <= 0 || students == NULL)
+		return NULL;
+	else{
+		int i, j;
+		struct student **result = (struct student **)malloc(sizeof(struct student)*K);
+		quicksort(students, 0, len - 1);
+		for (i = 0; i<K; i++)
+		{
+			result[i] = &students[i];
+		}
+
+		return result;
+	}
+
+}
+void quicksort(struct student *students, int first, int last){
+	int pivot, j, i;
+	struct student temp;
+
+	if (first<last){
+		pivot = first;
+		i = first;
+		j = last;
+
+		while (i<j){
+			while (students[i].score >= students[pivot].score&&i<last)
+				i++;
+			while (students[j].score<students[pivot].score)
+				j--;
+			if (i<j){
+				temp = students[i];
+				students[i] = students[j];
+				students[j] = temp;
+			}
+		}
+
+		temp = students[pivot];
+		students[pivot].score = students[j].score;
+		students[j] = temp;
+		quicksort(students, first, j - 1);
+		quicksort(students, j + 1, last);
+
+	}
 }
